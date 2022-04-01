e96563facf89cf73b97c99ccc80de54a841d3666
Justin <jusheinz@aol.de>
2022-04-02 00:32:40 +0100

    mobile works

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
-import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
 
 const shortenAddress = (address: string) => {
   return `${address.slice(0, 6)}...${address.slice(
     address.length - 4,
     address.length
   )}`;
-}
+};
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function Home() {
   const connector = useWalletConnect();
-
+  console.log();
   const connectWallet = React.useCallback(() => {
     return connector.connect();
   }, [connector]);
@@ -28,7 +24,11 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       {!connector.connected && (
         <TouchableOpacity onPress={connectWallet} style={styles.buttonStyle}>
           <Text style={styles.buttonTextStyle}>Connect a Wallet</Text>
@@ -49,17 +49,17 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   buttonStyle: {
     backgroundColor: "#3399FF",
