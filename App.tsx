5916685fa250fedf877c0e113611dd0051357587
Justin <jusheinz@aol.de>
2022-04-02 09:55:49 +0100

    changes

--- App.tsx
+++ App.tsx
@@ -3,10 +3,13 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { LoginSwitch } from "./src/screens/LoginSwitch";
 import { Provider as PaperProvider } from "react-native-paper";
 import { theme } from "./src/styles/theme";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
     <PaperProvider theme={theme}>
-      <LoginSwitch />
+      <NavigationContainer theme={theme}>
+        <LoginSwitch />
+      </NavigationContainer>
     </PaperProvider>
   );
 }
--- src/screens/Stack.tsx
+++ src/screens/Stack.tsx
@@ -17,40 +17,18 @@ const contractAddress = "";
 export const Stack = () => {
   const connector = useWalletConnect();
   const Stack = createNativeStackNavigator();
-  const killSession = useCallback(() => {
-    return connector.killSession();
-  }, [connector]);
-  const result = connector.sendTransaction({
-    from: String(connector.accounts[0]), // Required
-    to: "0x5AD22f3d353Ee97c041d5f504b1e77e2C766F3d2", // Required
-    //gas: "string", // Required
-    //gasPrice: "string", // Required
-    //value: "string", // Required
-    data: "0xcfae3217", // Required
-    //nonce: "string", // Required
-  });
-}, [connector])
-  /*
-  const contractCall = React.useCallback(() => {
-    console.log("Contract Call Happens Here")
-*/
-
-  /*
-  function testing(){
-    const testData = "0x53d9d9100000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000a6d9c5f7d4de3cef51ad3b7235d79ccc95114de5000000000000000000000000a6d9c5f7d4de3cef51ad3b7235d79ccc95114daa";
-    const decodedData = abiDecoder.decodeMethod(testData);
-    console.log(decodedData);
-  }*/
 
   return (
     <>
       <Text>{shortenAddress(connector.accounts[0])}</Text>
-      {/*<TouchableOpacity onPress={contractCall}>
-        <Text>Contract Call</Text>
-  </TouchableOpacity>*/}
-      <Button onPress={killSession}>
-        <Text>Log out</Text>
-      </Button>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="ItemList" component={ItemList} />
+        <Stack.Screen name="NewItem" component={NewItem} />
+      </Stack.Navigator>
     </>
   );
 };
