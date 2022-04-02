511ae5d406e3b9f63bfbc5ee25ff152e36efa7e3
Justin <jusheinz@aol.de>
2022-04-02 16:53:54 +0100

    before contract stuff

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -27,31 +27,3 @@ export const theme = {
     lightText: "#FFFFFF",
   },
 };
-
-export const carnoTheme = {
-  ...PaperDefaultTheme,
-  ...NavigationDefaultTheme,
-  roundness: 2,
-  dark: false,
-  colors: {
-    ...PaperDefaultTheme.colors,
-    ...NavigationDefaultTheme.colors,
-    primary: "#202532", //appbar colour
-    secondary: "#FFFFFF", //, //first menu colour
-    accent: "#eef2f5", //second menu colour
-    background: "#f8f9fa", //background for most stuff  "#a1c3e6"
-    surface: "#d8e0e6", //elevated surface colour (dropdown) and
-    text: "#000000", //text and icon standard
-    lightText: "#FFFFFF",
-  },
-  // #202532 Dark blue - background and header
-  // #37b877 Bright Green - Primary CTAs on the website are
-  // #f8f9fa Light grey background
-
-  // #eef2f5 Lighter grey 1
-  // #d8e0e6 Lighter grey
-
-  // #c2cdd4
-  // #aebcc6
-  // #9aabb8
-};
